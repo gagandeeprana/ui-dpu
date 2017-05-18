e95ae8e73eaa81ed6b322526831afa1a56d5df47
jagvir singh <erjagvirsingh92@gmail.com>
2017-05-18 16:45:27 +0530

    Regarding Driver issue

--- DPU_UI/src/com/dpu/model/Driver.java
+++ DPU_UI/src/com/dpu/model/Driver.java
@@ -4,10 +4,12 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
 
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 import org.codehaus.jackson.map.annotate.JsonSerialize;
 import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
 
 @JsonSerialize(include = Inclusion.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Driver implements Serializable{
 
 	 
